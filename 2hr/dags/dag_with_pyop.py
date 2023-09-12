fad1ac7fa30ad892fea8414f3200f9fe803b1b04
sunghj1118 <sunghj1118@gmail.com>
2023-09-13 01:21:46 +0900

    feat: use XCOMs to communicate between tasks

--- 2hr/dags/dag_with_pyop.py
+++ 2hr/dags/dag_with_pyop.py
@@ -9,33 +9,35 @@ default_args = {
     'retry_delay': timedelta(minutes=5)
 }
 
-def greet(name, age):
-    print(f"Hello World! My name is {name}, "
+def greet(age, ti):
+    first_name = ti.xcom_pull(task_ids='get_name', key= 'first_name')
+    last_name = ti.xcom_pull(task_ids='get_name', key= 'last_name')
+    print(f"Hello World! My name is {first_name} {last_name}, "
           f" and I am {age} years old.")
     
-def get_name():
-    return 'Minji'
+def get_name(ti):
+    ti.xcom_push(key='first_name', value='Hyunjoon')
+    ti.xcom_push(key='last_name', value='Sung')
 
 with DAG(
     default_args=default_args,
-    dag_id='dag_with_pyop_v3',
+    dag_id='dag_with_pyop_v5',
     description='This is our first dag that we write with python operator',
     start_date=datetime(2022, 9, 29, 2),
     schedule_interval='@daily'
 ) as dag:
-    # task1 = PythonOperator(
-    #     task_id='greet',
-    #     python_callable=greet,
-    #     op_kwargs={
-    #         'name': 'Hyunjoon',
-    #         'age': 23
-    #     }
-    # )
+    task1 = PythonOperator(
+        task_id='greet',
+        python_callable=greet,
+        op_kwargs={
+            'age': 23
+        }
+    )
 
     task2 = PythonOperator(
         task_id='get_name',
         python_callable=get_name
     )
 
-    task2
+    task2 >> task1
 
