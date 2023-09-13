bce0840842ea07e5dad8f7b470528af2fec57546
sunghj1118 <sunghj1118@gmail.com>
2023-09-13 15:44:08 +0900

    feat: use of catch-up to fill data

---
+++ 2hr/dags/catchup_and_backfill.py
@@ -0,0 +1,23 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+
+
+default_args = {
+    'owner': 'coder2j',
+    'retries': 5,
+    'retry_delay': timedelta(minutes=5)
+}
+
+with DAG(
+    dag_id='dag_with_catchup_backfill_v01',
+    default_args=default_args,
+    start_date=datetime(2023, 9, 10),
+    schedule_interval='@daily',
+    catchup=True
+) as dag:
+    task1 = BashOperator(
+        task_id='task1',
+        bash_command='echo This is a simple bash command!'
+    )
